70e8963c498733935c0b7ef5ee981f145f89775f
Lozano7 <lozanoj914@gmail.com>
2021-10-20 23:47:40 -0500

    arreglando interfaz

--- js/rules.js
+++ js/rules.js
@@ -2,10 +2,13 @@ const d = document;
 export default function viewRules() {
   d.addEventListener('click', (e) => {
     if (e.target.matches('#btn-rules')) {
+      d.documentElement.scrollTop = 0;
       d.querySelector('.rules').classList.add('active');
+      d.querySelector('body').classList.add('active-body');
     }
     if (e.target.matches('#cerrar *')) {
       d.querySelector('.rules').classList.remove('active');
+      d.querySelector('body').classList.remove('active-body');
     }
   });
 }
