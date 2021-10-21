10032b937033076ca50364932b8db1616f28745d
Lozano7 <lozanoj914@gmail.com>
2021-10-21 00:15:40 -0500

    arreglando interfaz de la ventana reglas

--- js/rules.js
+++ js/rules.js
@@ -2,12 +2,12 @@ const d = document;
 export default function viewRules() {
   d.addEventListener('click', (e) => {
     if (e.target.matches('#btn-rules')) {
-      d.documentElement.scrollTop = 0;
+      window.scrollTo(0, 0);
       d.querySelector('.rules').classList.add('active');
       d.querySelector('body').classList.add('active-body');
     }
     if (e.target.matches('#cerrar *')) {
-      d.documentElement.scrollTop = 0;
+      window.scrollTo(0, 0);
       d.querySelector('.rules').classList.remove('active');
       d.querySelector('body').classList.remove('active-body');
     }
