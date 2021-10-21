1fdc1b0f7e294fc57493a606c0786d359380c2d7
Lozano7 <lozanoj914@gmail.com>
2021-10-21 00:05:05 -0500

    arreglando interfaz

--- js/rules.js
+++ js/rules.js
@@ -7,6 +7,7 @@ export default function viewRules() {
       d.querySelector('body').classList.add('active-body');
     }
     if (e.target.matches('#cerrar *')) {
+      d.documentElement.scrollTop = 0;
       d.querySelector('.rules').classList.remove('active');
       d.querySelector('body').classList.remove('active-body');
     }
