4f8ea08e6f7c8a9cf01e9db26da1ff2c424f46a0
Lozano7 <lozanoj914@gmail.com>
2021-10-21 03:51:34 -0500

    eleccion de cartas de pc y usuario

---
+++ js/play.js
@@ -0,0 +1,59 @@
+import includeHtml from './include_html.js';
+
+const d = document;
+const $main = d.querySelector('.container-btn');
+
+export default function playNow() {
+  d.addEventListener('click', (e) => {
+    if (e.target.matches('.click')) {
+      establecerSonido(e.target.dataset.click);
+      setTimeout(() => {
+        includeHtml('.container-btn', 'assets/win_OR_loser.html');
+      }, 1000);
+      let random = Math.floor(Math.random() * (3 - 1) + 1).toString();
+      setTimeout(() => {
+        establecerPlayer('.user', e.target.dataset.click);
+        establecerPlayer('.rival', random);
+      }, 1030);
+    }
+  });
+}
+function establecerSonido(value) {
+  const options = {
+    1: '../assets/sounds/sonido-papel.mp3',
+    2: '../assets/sounds/sonido-tijera.mp3',
+    3: '../assets/sounds/sonido-piedra.mp3',
+  };
+  eligirSonido(options[value]);
+}
+function eligirSonido(src) {
+  const audio = d.createElement('audio');
+  audio.src = src;
+  audio.play();
+  setTimeout(() => {
+    audio.pause();
+    audio.currentTime = 0;
+  }, 1000);
+}
+function establecerPlayer(className, value) {
+  const $player = d.querySelector(className);
+  switch (value) {
+    case '1':
+      $player.style.backgroundColor = 'hsl(230, 89%, 62%)';
+      $player.querySelector('button img').src = '../images/icon-paper.svg';
+      break;
+    case '2':
+      $player.style.backgroundColor = 'hsl(39, 89%, 49%)';
+      $player.querySelector('button img').src = '../images/icon-scissors.svg';
+      break;
+
+    case '3':
+      $player.style.backgroundColor = 'hsl(349, 71%, 52%)';
+      $player.querySelector('button img').src = '../images/icon-rock.svg';
+      break;
+  }
+}
+
+function ganador(user, pc) {
+  let count = 0;
+}
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import includeHtml from './js/include_html.js';
+import playNow from './js/play.js';
 import viewRules from './js/rules.js';
 
 const d = document;
@@ -7,4 +8,5 @@ d.addEventListener('DOMContentLoaded', (e) => {
   includeHtml('.header', 'assets/header.html');
   includeHtml('.footer', 'assets/footer.html');
   viewRules();
+  playNow();
 });
